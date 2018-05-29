0d0b7be4fbf06932a166530e1b8e1f4a9d032ddc
junior serafim <junior.leles@hotmail.com>
2018-05-29 18:28:51 -0300

    App concluido

---
+++ Secao 05/App3/App3/App3/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App3.Service.Model;
+using App3.Service;
+
+namespace App3
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            BOTAO.Clicked += BuscarCEP;
+
+            
+		}
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            //todo logica do programa
+
+            //todo validacoes
+            string cep = CEP.Text.Trim();
+
+            if (isValidCep(cep))
+            {
+
+                try
+                {
+                    Endereco end = ViaCepService.BuscarEnderecoViaCep(cep);
+                    if (end != null)
+                    {
+                        RESULTADO.Text = string.Format("Endereço: {0}, {1} {2} {3}  ", end.localidade, end.uf, end.logradouro, end.bairro);
+                    }
+                    else
+                    {
+                        DisplayAlert("ERRO", "O Endereço não foi encontrado para o CEP informado" + cep, "OK");
+                    }
+                   
+
+                }
+                catch (Exception e)
+                {
+                    DisplayAlert("ERRO CRITICO", e.Message, "OK");
+                }
+            }
+           
+            
+        }
+
+        private bool isValidCep(string cep)
+        {
+            bool valido = true;
+
+            if(cep.Length != 8)
+            {
+
+                DisplayAlert("Erro", "CEP invalido! o cep deve conter 8 caracteres", "OK" );
+                valido = false;
+                //erro
+            }
+            int NovoCep = 0;
+
+            if(!int.TryParse(cep, out NovoCep))
+            {
+
+                DisplayAlert("Erro", "CEP invalido! o cep deve ser composto por numeros", "OK");
+                valido = false;
+                //erro
+            }
+            return valido;
+        }
+
+    }
+}
---
+++ Secao 05/App3/App3/App3/Service/ViaCepService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App3.Service.Model;
+using Newtonsoft.Json;
+
+namespace App3.Service
+{
+    public class ViaCepService
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+
+        public static Endereco BuscarEnderecoViaCep(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+
+            
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            if (end.cep == null) return null;
+
+            return end;
+
+
+
+
+            
+        }
+    }
+}
